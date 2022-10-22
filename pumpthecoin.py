770ab5af34641ec5ddc47de4dc996ddbda36e029
Daniel <tacombel@gmail.com>
2022-10-22 13:41:44 +0200

    Integrated the four books at southxchange

--- pumpthecoin.py
+++ pumpthecoin.py
@@ -1,8 +1,8 @@
 from importlib.util import find_spec
-from logging import PlaceHolder
 import requests
-import json
 import sys
+from config import Config
+from operator import itemgetter
 
 
 def btc_price():
@@ -17,6 +17,24 @@ def spc_price():
     spc = spc['siaprime-coin']['usd']
     return spc
 
+def usdt_price():
+    usdt = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=tether&vs_currencies=usd')
+    usdt = usdt.json()
+    usdt = usdt['tether']['usd']
+    return usdt
+
+def eth_price():
+    eth = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
+    eth = eth.json()
+    eth = eth['ethereum']['usd']
+    return eth
+
+def ltc_price():
+    ltc = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=litecoin&vs_currencies=usd')
+    ltc = ltc.json()
+    ltc = ltc['litecoin']['usd']
+    return ltc
+
 def data(target_price):
     target = target_price / btc_price()
     data = requests.get('https://www.southxchange.com/api/book/SCP/BTC')
@@ -29,59 +47,100 @@ def data(target_price):
             units += float(e['Amount'])
     return btc_price(), target, neccesary_amount, units
 
-def global_data():
+def get_orders(books): #books is a list of API addresses
+    buy_orders = []
+    sell_orders = []
+    for e in books:
+        type = e.split('/')[6]
+        if type == 'BTC':
+            price = btc_price()
+        elif type == 'USDT':
+            price = usdt_price()
+        elif type == 'LTC':
+            price = ltc_price()
+        elif type == 'ETH':
+            price = eth_price()
+        data = requests.get(e)
+        sellorders = data.json()['SellOrders']
+        buyorders = data.json()['BuyOrders']
+        for order in sellorders:
+            del order['Index']
+            order['Price'] = order['Price'] * price
+            order['Book'] = type
+            order['Value'] = order['Amount'] * order['Price']
+            sell_orders.append(order)
+        for order in buyorders:
+            del order['Index']
+            order['Price'] = order['Price'] * price
+            order['Book'] = type
+            order['Value'] = order['Amount'] * order['Price']
+            buy_orders.append(order)
+    buy_orders = sorted(buy_orders, key=itemgetter('Price'), reverse=True)
+    sell_orders = sorted(sell_orders, key=itemgetter('Price'), reverse=False)
+    print(f'Sell orders')
+    gap = ((sell_orders[0]['Price'] - buy_orders[0]['Price']) / buy_orders[0]['Price'] * 100)
+    for e in reversed(sell_orders[:10]):
+        print(e)
+    print(f'Gap: {gap:.2f}%')
+    print(f'Buy orders')
+    for e in buy_orders[:10]:
+        print(e)
+    return buy_orders, sell_orders
+
+
+def global_data(books):
     # total coins in circulation
     data = requests.get('https://consensus.scpri.me/status')
     total_coins = data.json()['totalcoins']
-    #data from southxchange
-    data = requests.get('https://www.southxchange.com/api/book/SCP/BTC')
-    sellorders = data.json()['SellOrders']
-    buyorders = data.json()['BuyOrders']
+    buyorders, sellorders = get_orders(books)
     units_in_sellorders_total = 0
     #sell orders
-    discard_factor = 10
     for e in sellorders:
         units_in_sellorders_total += float(e['Amount'])
     units_in_sellorders = 0
-    btc_in_sellorders = 0
+    dollars_in_sellorders = 0
     first_order = True
     for e in sellorders:
-        if not first_order and (float(e['Price'] > (btc_in_sellorders / units_in_sellorders * discard_factor))):
+        if not first_order and (float(e['Price'] > (dollars_in_sellorders / units_in_sellorders * Config.discard_factor))):
             break
         units_in_sellorders += float(e['Amount'])
-        btc_in_sellorders += float(e['Amount']) * float(e['Price'])
+        dollars_in_sellorders += float(e['Amount']) * float(e['Price'])
         first_order = False
         last_price_considered = float(e['Price'])
     #buy orders
     units_in_buyorders = 0
-    btc_in_buyorders = 0
+    dollars_in_buyorders = 0
     for e in buyorders:
         units_in_buyorders += float(e['Amount'])
-        btc_in_buyorders += float(e['Amount']) * float(e['Price'])
-    response = {'units_in_sell_orders': units_in_sellorders, 'btc_in_sellorders': btc_in_sellorders, 'units_in_buyorders': units_in_buyorders, 'btc_in_buyorders': btc_in_buyorders, 'units_in_sellorders_total': units_in_sellorders_total, 'total_coins': total_coins, 'last_price_considered': last_price_considered, 'discard_factor': discard_factor, 'price_spc_usd': spc_price(), 'btc': btc_price()}
+        dollars_in_buyorders += float(e['Amount']) * float(e['Price'])
+    response = {'units_in_sell_orders': units_in_sellorders, '$_in_sellorders': dollars_in_sellorders, 'units_in_buyorders': units_in_buyorders, '$_in_buyorders': dollars_in_buyorders, 'units_in_sellorders_total': units_in_sellorders_total, 'total_coins': total_coins, 'last_price_considered': last_price_considered, 'discard_factor': Config.discard_factor, 'price_spc_usd': spc_price(), 'price_btc_usd': btc_price(), 'price_eth_usd': eth_price(), 'price_ltc_usd': ltc_price()}
     
     return response
 
 if __name__ == "__main__":
+    # Pasar e como argumento para resumen del mercado o una cifra para pumpthecoin
     for index, e in enumerate(sys.argv):
         if index == 0:
             continue
         if e == 'e':
-            data = global_data()
+            # data from southxchange
+            # 'https://www.southxchange.com/api/book/SCP/BTC', 'https://www.southxchange.com/api/book/SCP/USDT', 'https://www.southxchange.com/api/book/SCP/ETH', 'https://www.southxchange.com/api/book/SCP/LTC'
+            books = ['https://www.southxchange.com/api/book/SCP/BTC', 'https://www.southxchange.com/api/book/SCP/USDT', 'https://www.southxchange.com/api/book/SCP/ETH', 'https://www.southxchange.com/api/book/SCP/LTC']
+            print(books)
+            data = global_data(['https://www.southxchange.com/api/book/SCP/BTC', 'https://www.southxchange.com/api/book/SCP/USDT', 'https://www.southxchange.com/api/book/SCP/ETH', 'https://www.southxchange.com/api/book/SCP/LTC'])
             btc = btc_price()
-            print(f'Data from Southxchange API, for the pair spc/BTC only.')
             print()
             print(f'SELL ORDERS')
             print(f'There are a total of {data["units_in_sellorders_total"]:.2f} coins (${data["units_in_sellorders_total"] * data["price_spc_usd"]:.2f}) in sell orders. This is {data["units_in_sellorders_total"] / data["total_coins"] * 100:.2f}% of the current coin in circulation.')
             print(f'To avoid distortion from orders with prices well above the rest, we will discard all remaining orders if the price of the order being considered is {data["discard_factor"]} times above the current average.')
-            print(f'The price at wich we stopped considering orders is {data["last_price_considered"]}btc (${data["last_price_considered"] * btc:.2f}).')
-            print(f'We will consider {data["units_in_sell_orders"] / data["units_in_sellorders_total"] * 100:.2f}% of the coins in the exchange. That is {data["units_in_sell_orders"]:.2f} coins in sell orders.')
-            print(f'The total asking price is {data["btc_in_sellorders"]:.2f} btc (${data["btc_in_sellorders"] * btc:.2f}) and the average is ${data["btc_in_sellorders"] / data["units_in_sell_orders"] * btc:.2f}/spc')
+            print(f'The price at which we stopped considering orders is ${data["last_price_considered"]}.')
+            print(f'We will consider {data["units_in_sell_orders"] / data["units_in_sellorders_total"] * 100:.2f}% of the coins at the exchange. That is {data["units_in_sell_orders"]:.2f} coins in sell orders.')
+            print(f'The total asking price is ${data["$_in_sellorders"]:.2f} and the average is ${data["$_in_sellorders"] / data["units_in_sell_orders"]:.2f}/spc')
             print()
             print('BUY ORDERS')
             print(f'There are {data["units_in_buyorders"]:.2f} coins in buy orders. This is {data["units_in_buyorders"] / data["total_coins"] * 100:.2f}% of the current coin in circulation.')
-            print(f'The total asking price is {data["btc_in_buyorders"]:.2f} btc (${data["btc_in_buyorders"] * btc:.2f})')
-            print(f'The average asking price is ${data["btc_in_buyorders"] / data["units_in_buyorders"] * btc:.2f}')
+            print(f'The total asking price is ${data["$_in_buyorders"]:.2f}')
+            print(f'The average asking price is ${data["$_in_buyorders"] / data["units_in_buyorders"]:.2f}')
         else:
             target_price = float(e)
             btc, target, neccesary_amount, units = data(target_price)
