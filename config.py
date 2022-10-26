17cc80fced609a38476a6cca3b0344d5d30dd22d
Daniel <tacombel@gmail.com>
2022-10-26 18:21:31 +0200

    Repaired environment variables

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY', default='nuncasabras')
-    discard_factor = os.getenv('SELL_DISCARD_FACTOR', default=5)
-    orders_listed = os.getenv('ORDERS_LISTED', default=10)
-    buy_order_limit = os.getenv('BUY_ORDER_LIMIT', default=80)
+    discard_factor = int(os.getenv('SELL_DISCARD_FACTOR', default=5))
+    orders_listed = int(os.getenv('ORDERS_LISTED', default=10))
+    buy_order_limit = int(os.getenv('BUY_ORDER_LIMIT', default=80))
