2a5263a7233914816a9f2a379279e2370804f0ab
Daniel <tacombel@gmail.com>
2023-03-29 19:51:51 +0200

    v0.9.10

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-v 0.9.9
+v 0.9.10
 
 Copy LICENSE, pumpthecoin.py and requirements.txt in you system. Ignore all other files.
 
--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t tacombel/pumpthecoin:v0.9.9 .
-docker push tacombel/pumpthecoin:v0.9.9
+docker build -t tacombel/pumpthecoin:v0.9.10 .
+docker push tacombel/pumpthecoin:v0.9.10
