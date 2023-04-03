2d09f40a4709e5373bd3e2755e55e4a486069c90
Daniel <tacombel@gmail.com>
2023-04-03 19:46:03 +0200

    First iteration to pumpthecoin

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,34 @@ import spf_earnings
 
 
 @app.route('/', methods=['GET'])
+@app.route('/pumpthecoin', methods=['GET', 'POST'])
+def pump_the_coin():
+        if request.method == 'GET':
+                return render_template('index.html', pumpthecoin_data=[0])
+        elif request.method == 'POST':
+                pump_the_coin = [0, 0, 0, 0, 0]
+                combine = []
+                if 'SXBTC' in request.form.getlist('market'):
+                        pump_the_coin[0] = 1
+                        combine.append(pumpthecoin.get_sx_orders('https://www.southxchange.com/api/book/SCP/BTC'))
+                if 'TOBTC' in request.form.getlist('market'):
+                        pump_the_coin[1] = 1
+                        combine.append(pumpthecoin.get_to_orders())
+                data = pumpthecoin.combine_data(combine)
+                if request.form['Price'] != '':
+                        amount = 0
+                        for e in data[1]:
+                                while float(e[4]) < float(request.form['Price']):
+                                        amount += e[2]
+                                else:
+                                        amount += 1
+                        pump_the_coin[2] = amount
+                elif request.form['amountToBuy'] != '':
+                        pump_the_coin[3] = request.form['amountToBuy']
+                elif request.form['amountToSell'] != '':
+                        pump_the_coin[4] = request.form['amountToSell']
+                return render_template('index.html', pumpthecoin_data=data)
+
 @app.route('/all', methods=['GET'])
 def all():
         data = pumpthecoin.combine_data([pumpthecoin.get_to_orders(), pumpthecoin.get_sx_orders('https://www.southxchange.com/api/book/SCP/BTC'), pumpthecoin.get_sx_orders('https://www.southxchange.com/api/book/SCP/USDT'), pumpthecoin.get_sx_orders('https://www.southxchange.com/api/book/SCP/ETH'), pumpthecoin.get_sx_orders('https://www.southxchange.com/api/book/SCP/LTC')])
