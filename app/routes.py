c9925b8d91ec2140bf793a1b940959022ec0fb4f
Daniel <tacombel@gmail.com>
2023-04-04 21:41:11 +0200

    seccion pumpthecoin practicamente terminada

--- app/routes.py
+++ app/routes.py
@@ -23,13 +23,11 @@ def pump_the_coin():
                 ask = data[1]
                 ask.reverse()
                 if request.form['Price'] != '':
-                        pump_the_coin['query'] = 'price'
                         pump_the_coin['price'] = float(request.form['Price'])
                         dollars = 0
                         amount = 0
                         order = 1
                         for e in ask:
-                                
                                 if float(e[4]) < float(request.form['Price']):
                                         dollars += e[5]
                                         amount += e[2]
@@ -38,13 +36,31 @@ def pump_the_coin():
                                 order += 1
                         if order > len(ask):
                                 pump_the_coin['last_price'] = ask[-1][4]
-                        
                         pump_the_coin['dollars'] = round(dollars)
                         pump_the_coin['amount'] = round(amount)
                 elif request.form['amountToBuy'] != '':
-                        a = request.form['amountToBuy']
+                        dollars = 0
+                        amount = 0
+                        order = 1
+                        for e in ask:
+                                if amount + e[2] < float(request.form['amountToBuy']):
+                                        dollars += e[5]
+                                        amount += e[2]
+                                else:
+                                        rest = float(request.form['amountToBuy']) - amount
+                                        amount += rest
+                                        dollars += rest * e[4]
+                                        print(f'else {amount} {dollars}')
+                                        break
+                                order += 1
+                        pump_the_coin['amount_buying'] = round(float(request.form['amountToBuy']))
+                        if order > len(ask):
+                                pump_the_coin['last_price_buying'] = ask[-1][4]
+                                pump_the_coin['amount_buying'] = round(amount)
+                        pump_the_coin['dollars_buying'] = round(dollars)
+                        pump_the_coin['average_buying'] = round(dollars / float(request.form['amountToBuy']), 2)
                 elif request.form['amountToSell'] != '':
-                        b = request.form['amountToSell']
+                        pump_the_coin['amountToSell'] = 'No way'
                 return render_template('index.html', pumpthecoin_data=pump_the_coin)
 
 @app.route('/all', methods=['GET'])
