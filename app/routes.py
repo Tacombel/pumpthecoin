1220260ec74e8f46230d002c0c0943285a65c100
Daniel <tacombel@gmail.com>
2023-04-05 20:08:25 +0200

    Bad input control

--- app/routes.py
+++ app/routes.py
@@ -13,55 +13,78 @@ def pump_the_coin():
         elif request.method == 'POST':
                 pump_the_coin = {}
                 combine = []
-                if 'SXBTC' in request.form.getlist('market'):
-                        pump_the_coin[0] = 1
-                        combine.append(pumpthecoin.get_sx_orders('https://www.southxchange.com/api/book/SCP/BTC'))
-                if 'TOBTC' in request.form.getlist('market'):
-                        pump_the_coin[1] = 1
-                        combine.append(pumpthecoin.get_to_orders())
-                data = pumpthecoin.combine_data(combine)
-                ask = data[1]
-                ask.reverse()
-                if request.form['Price'] != '':
-                        pump_the_coin['price'] = float(request.form['Price'])
-                        dollars = 0
-                        amount = 0
-                        order = 1
-                        for e in ask:
-                                if float(e[4]) < float(request.form['Price']):
-                                        dollars += e[5]
-                                        amount += e[2]
-                                else:
-                                        break
-                                order += 1
-                        if order > len(ask):
-                                pump_the_coin['last_price'] = round(ask[-1][4], 4)
-                        pump_the_coin['dollars'] = round(dollars)
-                        pump_the_coin['amount'] = round(amount)
-                        pump_the_coin['average'] = round(dollars / amount, 4)
-                elif request.form['amountToBuy'] != '':
-                        dollars = 0
-                        amount = 0
-                        order = 1
-                        for e in ask:
-                                if amount + e[2] < float(request.form['amountToBuy']):
-                                        dollars += e[5]
-                                        amount += e[2]
-                                else:
-                                        rest = float(request.form['amountToBuy']) - amount
-                                        amount += rest
-                                        dollars += rest * e[4]
-                                        print(f'else {amount} {dollars}')
-                                        break
-                                order += 1
-                        pump_the_coin['amount_buying'] = round(float(request.form['amountToBuy']))
-                        if order > len(ask):
-                                pump_the_coin['last_price_buying'] = round(ask[-1][4], 4) #used as key in the html
-                                pump_the_coin['amount_buying'] = round(amount)
-                        pump_the_coin['dollars_buying'] = round(dollars)
-                        pump_the_coin['average_buying'] = round(dollars / float(request.form['amountToBuy']), 2)
-                elif request.form['amountToSell'] != '':
-                        pump_the_coin['amountToSell'] = 'No way'
+                if 'SXBTC' in request.form.getlist('market') or 'TOBTC' in request.form.getlist('market'):
+                        if 'SXBTC' in request.form.getlist('market'):
+                                pump_the_coin[0] = 1
+                                combine.append(pumpthecoin.get_sx_orders('https://www.southxchange.com/api/book/SCP/BTC'))
+                        if 'TOBTC' in request.form.getlist('market'):
+                                pump_the_coin[1] = 1
+                                combine.append(pumpthecoin.get_to_orders())
+                        data = pumpthecoin.combine_data(combine)
+                        ask = data[1]
+                        ask.reverse()
+                        if request.form['Price'] != '':
+                                pump_the_coin['price'] = float(request.form['Price'])
+                                dollars = 0
+                                amount = 0
+                                order = 1
+                                for e in ask:
+                                        if float(e[4]) < float(request.form['Price']):
+                                                dollars += e[5]
+                                                amount += e[2]
+                                        else:
+                                                break
+                                        order += 1
+                                if order > len(ask):
+                                        pump_the_coin['last_price'] = round(ask[-1][4], 4)
+                                pump_the_coin['dollars'] = round(dollars)
+                                pump_the_coin['amount'] = round(amount)
+                                pump_the_coin['average'] = round(dollars / amount, 4)
+                        elif request.form['amountToBuy'] != '':
+                                dollars = 0
+                                amount = 0
+                                order = 1
+                                for e in ask:
+                                        if amount + e[2] < float(request.form['amountToBuy']):
+                                                dollars += e[5]
+                                                amount += e[2]
+                                        else:
+                                                rest = float(request.form['amountToBuy']) - amount
+                                                amount += rest
+                                                dollars += rest * e[4]
+                                                break
+                                        order += 1
+                                pump_the_coin['amount_buying'] = round(float(request.form['amountToBuy']))
+                                if order > len(ask):
+                                        pump_the_coin['last_price_buying'] = round(ask[-1][4], 4) #used as key in the html
+                                        pump_the_coin['amount_buying'] = round(amount)
+                                pump_the_coin['dollars_buying'] = round(dollars)
+                                pump_the_coin['average_buying'] = round(dollars / float(request.form['amountToBuy']), 2)
+                        elif request.form['amountToSell'] != '':
+                                bid = data[0]
+                                dollars = 0
+                                amount = 0
+                                order = 1
+                                for e in bid:
+                                        if amount + e[2] < float(request.form['amountToSell']):
+                                                print(e)
+                                                dollars += e[5]
+                                                amount += e[2]
+                                        else:
+                                                print(e)
+                                                rest = float(request.form['amountToSell']) - amount
+                                                amount += rest
+                                                dollars += rest * e[4]
+                                                break
+                                        order += 1
+                                pump_the_coin['amount_selling'] = round(float(request.form['amountToSell']))
+                                if order > len(bid):
+                                        pump_the_coin['last_price_selling'] = round(ask[-1][4], 4) #used as key in the html
+                                        pump_the_coin['amount_selling'] = round(amount)
+                                pump_the_coin['dollars_selling'] = round(dollars)
+                                pump_the_coin['average_selling'] = round(dollars / float(request.form['amountToSell']), 2)
+                else:
+                        pump_the_coin['error'] = 'You need to select at least one market'
                 return render_template('index.html', pumpthecoin_data=pump_the_coin)
 
 @app.route('/all', methods=['GET'])
