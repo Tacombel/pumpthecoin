6320a00a2fdd2fd454aaad571979077bdc2c7c6c
Daniel <tacombel@gmail.com>
2023-04-09 20:26:22 +0200

    v0.12.6

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-tacombel/pumpthecoin:v0.12.5
+tacombel/pumpthecoin:v0.12.6
 
 -----------------------------------------------
 
--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t tacombel/pumpthecoin:v0.12.5 .
-docker push tacombel/pumpthecoin:v0.12.5
+docker build -t tacombel/pumpthecoin:v0.12.6 .
+docker push tacombel/pumpthecoin:v0.12.6
