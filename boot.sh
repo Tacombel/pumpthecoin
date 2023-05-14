40b4121f84816d7e7d63bb976937aa3bb3cf3b88
Daniel <tacombel@gmail.com>
2023-05-14 17:32:35 +0200

    workers=1

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source venv/bin/activate
-exec gunicorn --workers=3 -b :5200 --access-logfile - --error-logfile - pumpthecoin-flask:app
+exec gunicorn --workers=1 -b :5200 --access-logfile - --error-logfile - pumpthecoin-flask:app
