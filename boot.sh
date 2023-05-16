80825de0bd380be52424d09345e113d8c91a444a
Daniel <tacombel@gmail.com>
2023-05-16 18:29:34 +0200

    v0.12.6c3

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source venv/bin/activate
-exec gunicorn --workers=1 -b :5200 --access-logfile - --error-logfile - pumpthecoin-flask:app
+exec gunicorn --config gunicorn_config.py --access-logfile - --error-logfile - pumpthecoin-flask:app
