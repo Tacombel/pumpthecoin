e28cddfc3b57beccae9de9e463d6798c80d9a18d
Daniel <tacombel@gmail.com>
2023-05-17 18:55:46 +0200

    Added db csv

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t tacombel/pumpthecoin:v0.12.6c5 .
-docker push tacombel/pumpthecoin:v0.12.6c5
+docker build -t tacombel/pumpthecoin:v0.12.6c6 .
+docker push tacombel/pumpthecoin:v0.12.6c6
--- update_balance.py
+++ update_balance.py
@@ -1,4 +1,5 @@
 import sqlite3
+import csv
 from contest import get_data
 
 def get_balances():
@@ -12,5 +13,20 @@ def get_balances():
             conn.commit()
     conn.close()
 
+def write_csv():
+    conn = sqlite3.connect("./contest/app.db")
+    cur = conn.cursor()
+    sql = 'SELECT discord_user, users.nickname, users.hash, amount from users INNER JOIN balance ON users.hash = balance.hash;'
+    cur.execute(sql)
+    result = cur.fetchall()
+    
+    with open('./contest/db_backup.csv', mode='w') as csv_file:
+        fieldnames = ['discord_user', 'nickname', 'hash', 'amount']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in result:
+            writer.writerow({'discord_user':row[0], 'nickname':row[1], 'hash':row[2], 'amount':row[3]})
+
 if __name__ == "__main__":
     get_balances()
+    write_csv()
