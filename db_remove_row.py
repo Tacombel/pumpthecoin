7d0efea221108e02d79e72bdcaba68a2dcdb266d
Daniel <tacombel@gmail.com>
2023-05-17 17:59:12 +0200

    Also delete user from balance

--- db_remove_row.py
+++ db_remove_row.py
@@ -5,8 +5,10 @@ hash = sys.argv[1]
 print(f'Deleting {hash} from the database.')
 
 conn = sqlite3.connect("./contest/app.db")
-sql = 'DELETE FROM USERS WHERE hash=?'
 cur = conn.cursor()
+sql = 'DELETE FROM users WHERE hash=?'
+cur.execute(sql, (hash,))
+sql = 'DELETE FROM balance WHERE hash=?'
 cur.execute(sql, (hash,))
 conn.commit()
 conn.close()
