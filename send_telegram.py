f8c6bf368e45675c65e57ab9b646f3512e6bb894
Daniel <tacombel@gmail.com>
2023-06-01 20:31:56 +0200

    logging

--- send_telegram.py
+++ send_telegram.py
@@ -7,6 +7,7 @@ def send_telegram_msg(message):
     bot_chatID = Config.user_id
     bot_message = message
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=html&text=' + bot_message
+    print(send_text)
     response = requests.get(send_text)
     return response.json()
 
