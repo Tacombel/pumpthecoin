aab1e9ec9a24e9b8d4ea0faf9389f630e94ff1c8
Daniel <tacombel@gmail.com>
2023-06-01 20:40:38 +0200

    Removed logging

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t tacombel/pumpthecoin:v0.12.6c11 .
-docker push tacombel/pumpthecoin:v0.12.6c11
+docker build -t tacombel/pumpthecoin:v0.12.6c12 .
+docker push tacombel/pumpthecoin:v0.12.6c12
