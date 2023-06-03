d35c83791745ec1c728541a868fa1aae51e46bd1
Daniel <tacombel@gmail.com>
2023-06-03 09:40:37 +0200

    v0.12.6c16

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t tacombel/pumpthecoin:v0.12.6c15 .
-docker push tacombel/pumpthecoin:v0.12.6c15
+docker build -t tacombel/pumpthecoin:v0.12.6c16 .
+docker push tacombel/pumpthecoin:v0.12.6c16
