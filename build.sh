ca84385a6b8f4794758872dab7f41b64c8bfef62
tacombel <tacombel@gmail.com>
2023-06-12 18:01:40 +0200

    v0.12.6c34-a

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t tacombel/pumpthecoin:v0.12.6c33-a .
-docker push tacombel/pumpthecoin:v0.12.6c33-a
+docker build -t tacombel/pumpthecoin:v0.12.6c34-a .
+docker push tacombel/pumpthecoin:v0.12.6c34-a
