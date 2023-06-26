c0eb78c5ea11d2e43f123c4af62b6b4802fd7834
tacombel <tacombel@gmail.com>
2023-06-26 15:58:27 +0200

    v0.12.6c38-a

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-tacombel/pumpthecoin:v0.12.6c37-a
+tacombel/pumpthecoin:v0.12.6c38-a
 
 Instalacion en un nuevo ordenador con VS Code
 
--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t tacombel/pumpthecoin:v0.12.6c37-a .
-docker push tacombel/pumpthecoin:v0.12.6c37-a
+docker build -t tacombel/pumpthecoin:v0.12.6c38-a .
+docker push tacombel/pumpthecoin:v0.12.6c38-a
